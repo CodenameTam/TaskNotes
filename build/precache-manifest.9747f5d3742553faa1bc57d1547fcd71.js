5bc4ae23899923546f6fee927e099bf6464b7bd5
CodeNameTam <CodeNameTam@yahoo.com>
2021-01-09 12:38:12 -0500

    Additions

---
+++ build/precache-manifest.9747f5d3742553faa1bc57d1547fcd71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad44df8136faaa5a1bd7c3f06ae199e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f72922834687c8defa8",
+    "url": "/static/js/2.f9d985c8.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/static/js/2.f9d985c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306d2c92fcda25409a83",
+    "url": "/static/js/main.3413aeb7.chunk.js"
+  },
+  {
+    "revision": "c96420b2c9f1b6fe6c64",
+    "url": "/static/js/runtime-main.47214ee2.js"
+  }
+]);
